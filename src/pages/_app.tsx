3d291b37f63d5a3d18501c16920b401bb8ef089c
Dávid Kopáčik <david.kopacik@gmail.com>
2023-08-24 12:57:52 +0200

    added _app.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { getLayout } from "@vercel/examples-ui";
+import "@vercel/examples-ui/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<any>(Component);
+
+  return (
+    <Layout
+      title="Password Protected"
+      path="edge-middleware/basic-auth-password"
+    >
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
